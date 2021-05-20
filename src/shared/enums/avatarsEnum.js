cc8c1c8487e89edb3c365f7ac56cbad2e8566d51
stevescruz <steve.silverio.cruz@gmail.com>
2021-05-20 17:40:31 -0400

    REF: Abstracted Enums from App -Created an enum with the available avatar image options. -Created an enum with the available player name options. -Created an enum with the combinations responsible for winning a match.

---
+++ src/shared/enums/avatarsEnum.js
@@ -0,0 +1,9 @@
+const AVATARS_ENUM = {
+  1: 'assets/Avatar_Cat-512.png',
+  2: 'assets/Avatar_Dog-512.png',
+  3: 'assets/Avatar_Panda-512.png',
+  4: 'assets/Avatar_Rabbit-512.png',
+  5: 'assets/Avatar_Pig-512.png',
+}
+
+export { AVATARS_ENUM };
---
+++ src/shared/enums/playerNamesEnum.js
@@ -0,0 +1,14 @@
+const PLAYER_NAMES_ENUM = {
+  1: 'Chewbarka',
+  2: 'Ham Solo',
+  3: 'Pandaren',
+  4: 'Skyhopper',
+  5: 'Emperor',
+  6: 'JK Growling',
+  7: 'Chris P. Bacon',
+  8: 'Expandable',
+  9: 'Cinnabun',
+  10: 'Cameow',
+};
+
+export { PLAYER_NAMES_ENUM };
