212fb3aeee288164e163973b2412ec106547db57
stevescruz <steve.silverio.cruz@gmail.com>
2021-05-24 16:46:54 -0400

    FIX: Fixed generateRandomNumber to be inclusive with min and max

--- src/shared/utils/generateRandomNumber.js
+++ src/shared/utils/generateRandomNumber.js
@@ -1,5 +1,5 @@
 function generateRandomNumber(min, max) {
-  return Math.floor((Math.random() * max) + min);
+  return Math.floor((Math.random() * (max - min + 1)) + min);
 }
 
 export {generateRandomNumber};
